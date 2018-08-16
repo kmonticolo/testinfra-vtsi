6ee1faa3e182b9c89aab09abeecf6ee2ab29ca91
Kamil Monticolo <kmonticolo@dcclabs.tv>
2018-08-16 08:32:20 +0200

    change mhrtc to process.get

--- test_pump.py
+++ test_pump.py
@@ -86,7 +86,9 @@ def test_CCURmhrtc_service_exists(host, Process, Socket):
     service = host.service("CCURmhrtc")
     assert service.is_enabled
     assert service.is_running
-    mhrtc = Process.filter(comm="mhrtc")
+    mhrtc = Process.get(comm="mhrtc")
+    assert mhrtc.user == "root"
+    assert mhrtc.group == "root"
     assert Socket("tcp://127.0.0.1:8062").is_listening
     assert Socket("tcp://0.0.0.0:8037").is_listening
     assert Socket("tcp://0.0.0.0:8039").is_listening
