4f95e9d62fc912655cbdeecca9d7b9ad41b56d64
Kamil Monticolo <kmonticolo@dcclabs.tv>
2018-08-16 08:44:24 +0200

    add test_cim.py

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@
 py.test test_vtsi.py --ssh-config=/home/kamil/.ssh/config --hosts root@10.48.77.31 -v
 
 py.test test_pump.py --ssh-config=/home/kamil/.ssh/config --hosts root@10.48.77.30 -v
+
+py.test test_cim.py --ssh-config=/home/kamil/.ssh/config --hosts root@10.48.77.29 -v
+
