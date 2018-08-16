54d57bfd97ff1adbbf0f36ea180de46572ca3fa7
Kamil Monticolo <kmonticolo@dcclabs.tv>
2018-08-16 11:26:06 +0200

    add detailed gssproxy.conf

--- test_pump.py
+++ test_pump.py
@@ -204,8 +204,6 @@ def test_httpsmd_running(host, Process, Service, Socket, Command):
     assert host.file("/etc/opt/MediaHawk/httpsm.cfg").contains("Log-Level = 20")
     assert host.file("/etc/opt/MediaHawk/httpsm.cfg").contains("Log-FilePath = /var/log/httpsm.log")
 
-
-
 def test_gssproxy_running(host, File, Process, Service, Socket, Command):
     gssproxy = Process.get(ppid='1', comm="gssproxy")
     assert gssproxy.user == "root"
@@ -226,6 +224,22 @@ def test_gssproxy_running(host, File, Process, Service, Socket, Command):
     assert host.file("/etc/gssproxy/gssproxy.conf").is_file
     assert host.file("/etc/gssproxy/gssproxy.conf").user == 'root'
     assert host.file("/etc/gssproxy/gssproxy.conf").group == 'root'
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("mechs = krb5")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("cred_store = keytab:/etc/gssproxy/http.keytab")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("cred_store = ccache:/var/lib/gssproxy/clients/krb5cc_%U")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("euid = 48")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("mechs = krb5")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("socket = /run/gssproxy.sock")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("cred_store = keytab:/etc/krb5.keytab")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("trusted = yes")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("kernel_nfsd = yes")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("euid = 0")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("cred_store = keytab:/etc/krb5.keytab")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("cred_store = ccache:FILE:/var/lib/gssproxy/clients/krb5cc_%U")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("cred_store = client_keytab:/var/lib/gssproxy/clients/%U.keytab")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("cred_usage = initiate")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("allow_any_uid = yes")
+    assert host.file("/etc/gssproxy/gssproxy.conf").contains("trusted = yes")
 
 
 
